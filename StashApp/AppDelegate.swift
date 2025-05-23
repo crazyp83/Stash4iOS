c3a43d24c5595764ea144e1c95b8612d27fd8dbf
crazyp83 <crazyp83@users.noreply.github.com>
2025-05-22 20:05:04 -0400

    v1

--- StashApp/AppDelegate.swift
+++ StashApp/AppDelegate.swift
@@ -6,16 +6,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        
-        let defaults = UserDefaults.standard
-        let rootVC: UIViewController
-        if defaults.string(forKey: "address") == nil {
-            rootVC = SettingsViewController()
-        } else {
-            rootVC = MainViewController()
-        }
-        
-        window?.rootViewController = rootVC
+        window?.rootViewController = ViewController()
         window?.makeKeyAndVisible()
         return true
     }
---
+++ StashApp/ViewController.swift
@@ -0,0 +1,25 @@
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    var webView: WKWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        webView = WKWebView()
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(webView)
+
+        // Auto Layout constraints
+        NSLayoutConstraint.activate([
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+
+        if let url = URL(string: "https://stash.doylerules.org") {
+            webView.load(URLRequest(url: url))
+        }
+    }
+}
