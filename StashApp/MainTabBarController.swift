6a7145bf481d0bdd5178a9e6dd23db5da349fe98
crazyp83 <crazyp@icloud.com>
2026-05-20 19:22:48 -0400

    feat: Complete native tab bar - all 6 tabs now use real GraphQL views (Scenes, Performers, Galleries, Images, Studios)

--- StashApp/MainTabBarController.swift
+++ StashApp/MainTabBarController.swift
@@ -13,31 +13,15 @@ class MainTabBarController: UITabBarController {
         let performersVC = PerformersViewController()
         performersVC.tabBarItem = UITabBarItem(title: "Performers", image: UIImage(systemName: "person.2"), tag: 0)
         
-        let galleriesVC = createPlaceholder(title: "Galleries", icon: "photo.on.rectangle")
-        let imagesVC = createPlaceholder(title: "Images", icon: "photo")
-        let studiosVC = createPlaceholder(title: "Studios", icon: "building.2")
+        let galleriesVC = GalleriesViewController()
+        galleriesVC.tabBarItem = UITabBarItem(title: "Galleries", image: UIImage(systemName: "photo.on.rectangle"), tag: 0)
+        
+        let imagesVC = ImagesViewController()
+        imagesVC.tabBarItem = UITabBarItem(title: "Images", image: UIImage(systemName: "photo"), tag: 0)
+        
+        let studiosVC = StudiosViewController()
+        studiosVC.tabBarItem = UITabBarItem(title: "Studios", image: UIImage(systemName: "building.2"), tag: 0)
         
         viewControllers = [webVC, scenesVC, performersVC, galleriesVC, imagesVC, studiosVC]
-    }
-    
-    private func createPlaceholder(title: String, icon: String) -> UIViewController {
-        let vc = UIViewController()
-        vc.view.backgroundColor = .systemBackground
-        
-        let label = UILabel()
-        label.text = "\(title) tab coming soon\n(Native GraphQL view will go here)"
-        label.numberOfLines = 0
-        label.textAlignment = .center
-        label.font = .preferredFont(forTextStyle: .title2)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
-        vc.view.addSubview(label)
-        NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: vc.view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: vc.view.centerYAnchor)
-        ])
-        
-        vc.tabBarItem = UITabBarItem(title: title, image: UIImage(systemName: icon), tag: 0)
-        return vc
     }
 }
