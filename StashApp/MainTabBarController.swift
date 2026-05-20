8dcf3fc9ee4f350a69ec7b84366edfb236b7c467
crazyp83 <crazyp@icloud.com>
2026-05-20 19:05:00 -0400

    feat: Add native UITabBarController with Scenes, Performers, Galleries, Images, Studios tabs (Web tab uses existing ViewController)

---
+++ StashApp/MainTabBarController.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let webVC = ViewController()
+        webVC.tabBarItem = UITabBarItem(title: "Web", image: UIImage(systemName: "globe"), tag: 0)
+        
+        let scenesVC = createPlaceholder(title: "Scenes", icon: "film")
+        let performersVC = createPlaceholder(title: "Performers", icon: "person.2")
+        let galleriesVC = createPlaceholder(title: "Galleries", icon: "photo.on.rectangle")
+        let imagesVC = createPlaceholder(title: "Images", icon: "photo")
+        let studiosVC = createPlaceholder(title: "Studios", icon: "building.2")
+        
+        viewControllers = [webVC, scenesVC, performersVC, galleriesVC, imagesVC, studiosVC]
+    }
+    
+    private func createPlaceholder(title: String, icon: String) -> UIViewController {
+        let vc = UIViewController()
+        vc.view.backgroundColor = .systemBackground
+        
+        let label = UILabel()
+        label.text = "\(title) tab coming soon\n(Native GraphQL view will go here)"
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.font = .preferredFont(forTextStyle: .title2)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        vc.view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: vc.view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: vc.view.centerYAnchor)
+        ])
+        
+        vc.tabBarItem = UITabBarItem(title: title, image: UIImage(systemName: icon), tag: 0)
+        return vc
+    }
+}
